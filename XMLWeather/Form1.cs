b246ccbff1eae7a595555f6276dbe3cfbfa762f5
Miguel Montoya <migumont644@ed.amdsb.ca>
2022-04-20 09:49:20 -0400

    changes

--- XMLWeather/Form1.cs
+++ XMLWeather/Form1.cs
@@ -14,7 +14,7 @@ namespace XMLWeather
     public partial class Form1 : Form
     {
         // TODO: create list to hold day objects
-
+       public static List<Day> days = new List<Day>();
 
         public Form1()
         {
@@ -34,11 +34,24 @@ namespace XMLWeather
 
             while (reader.Read())
             {
-                //TODO: create a day object
+                // Create a day object
+                Day newDay = new Day();
 
-                //TODO: fill day object with required data
+                //Fill day object with required data
+                reader.ReadToFollowing("time");
+                newDay.date = reader.GetAttribute("day");
 
-                //TODO: if day object not null add to the days list
+                reader.ReadToFollowing("temperature");
+                newDay.tempLow = reader.GetAttribute("min");
+
+                //if day object not null add to the days list
+                if (newDay.date != null)
+                {
+                    days.Add(newDay);
+                }
+
+
+
             }
         }
 
@@ -47,8 +60,12 @@ namespace XMLWeather
             // current info is not included in forecast file so we need to use this file to get it
             XmlReader reader = XmlReader.Create("http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0");
 
-            //TODO: find the city and current temperature and add to appropriate item in days list
+            //Find the city and current temperature and add to appropriate item in days list
+            reader.ReadToFollowing("city");
+            days[0].location = reader.GetAttribute("name");
 
+            reader.ReadToFollowing("temperture");
+            days[0].currentTemp = reader.GetAttribute("value");
         }
 
 
